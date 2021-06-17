8a06b7ddd0e8cef5605eeb9191a873d7e92539fb
minwoos2 <73131628+minwoos2@users.noreply.github.com>
2021-06-18 03:28:05 +0900

    message here

---
+++ CCTM/scripts/newscript.jl
@@ -0,0 +1,31 @@
+using NetCDF_jll, NetCDFF_jll, IOAPI_jll, MPICH_jll
+
+basedir = joinpath(dirname(@__FILE__), "../..")
+if basedir == "../.."
+    basedir = @__DIR__
+end
+
+"running in: $basedir"
+
+
+scriptdir = joinpath(basedir, "CCTM", "scripts")
+cd(scriptdir)
+
+withenv( 
+    "IOAPI_LIB_DIR"=>joinpath(IOAPI_jll.artifact_dir, "lib"),
+    "IOAPI_INCL_DIR"=>joinpath(IOAPI_jll.artifact_dir, "include"),
+    "NETCDF_LIB_DIR"=>joinpath(NetCDF_jll.artifact_dir, "lib"),
+    "NETCDF_INCL_DIR"=>joinpath(NetCDF_jll.artifact_dir, "include"),
+    "NETCDFF_LIB_DIR"=>joinpath(NetCDFF_jll.artifact_dir, "lib"),
+    "NETCDFF_INCL_DIR"=>joinpath(NetCDFF_jll.artifact_dir, "include"),
+    #"MPI_LIB_DIR"=>joinpath(basedir, mpitemp),
+    "PATH"=>ENV["PATH"]*":"*MPICH_jll.PATH,
+    "LIBRARY_PATH"=>MPICH_jll.LIBPATH*":"*IOAPI_jll.LIBPATH[],
+    MPICH_jll.LIBPATH_env=>MPICH_jll.LIBPATH*":"*IOAPI_jll.LIBPATH[],
+    "RPATH"=>MPICH_jll.LIBPATH*":"*IOAPI_jll.LIBPATH[],
+    ) do
+
+    run(`./run_cctm_Bench_2016_12SE1.csh`)
+
+
+end
